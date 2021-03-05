7dfcbd9e445d6a3b20e15a1266dfcc8772a6d8ba
Balance8 <mslocum819@gmail.com>
2021-03-04 17:03:26 -0800

    color changes, tailwind config update, and adding components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 // pages/index.js
 import { useUser } from '@auth0/nextjs-auth0';
 import { useTheme } from 'next-themes';
+import Layout from 'src/components/layout';
 
 const index = () => {
   const { user, error, isLoading } = useUser();
@@ -9,30 +10,30 @@ const index = () => {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
-  if (user) {
-    return (
-      <>
-        <nav className='z-50 w-full h-16 bg-purple dark:bg-darkgrey'>
-          <button
-            aria-label='Toggle Dark Mode'
-            type='button'
-            className='order-2 w-12 h-12 p-3 md:order-3'
-            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-          >
-            test
-          </button>
-        </nav>
-        <div className='container mx-auto dark:text-blue-900'>
-          Welcome {user.name}!
-        </div>
-        <a className='container mx-auto' href='/api/auth/logout'>
-          Logout
-        </a>
-      </>
-    );
-  }
-
-  return <a href='/api/auth/login'>Login</a>;
+  return (
+    <Layout
+      main={
+        <>
+          {/* <nav className='z-50 w-full h-16 bg-purple dark:bg-darkgrey'>
+              <button
+                aria-label='Toggle Dark Mode'
+                type='button'
+                className='order-2 w-12 h-12 p-3 md:order-3'
+                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+              >
+                test
+              </button>
+            </nav>
+            <div className='container mx-auto dark:text-blue-900'>
+              Welcome {user.name}!
+            </div>
+            <a className='container mx-auto' href='/api/auth/logout'>
+              Logout
+            </a> */}
+        </>
+      }
+    ></Layout>
+  );
 };
 
 export default index;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,82 @@
 module.exports = {
-  purge: [],
-  darkMode: 'class', // or 'media' or 'class'
+  important: true,
+  // Active dark mode on class basis
+  darkMode: 'class',
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+  purge: {
+    content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+    // These options are passed through directly to PurgeCSS
+  },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#bcbec2',
+          normal: '#202634',
+          dark: '#131721',
+          button: '#475473',
+        },
+        accent: {
+          light: '#b8babc',
+          normal: '#121821',
+          dark: '#0a0e14',
+        },
+        warning: {
+          light: '#f0c2b3',
+          normal: '#cc3300',
+          dark: '#b92000',
+        },
+        textColor: {
+          light: '#000000',
+          dark: '#ffffff',
+        },
+        backgroundColor: {
+          light: '#fafafa',
+          dark: '#202634',
+        },
+        vulcan: {
+          DEFAULT: '#121821',
+          50: '#6B86AE',
+          100: '#5A78A5',
+          200: '#486084',
+          300: '#364863',
+          400: '#243042',
+          500: '#121821',
+          600: '#000000',
+        },
+        charade: {
+          DEFAULT: '#202634',
+          50: '#f4f4f5',
+          100: '#e9e9eb',
+          200: '#c7c9cc',
+          300: '#a6a8ae',
+          400: '#636771',
+          500: '#202634',
+          600: '#1d222f',
+          700: '#181d27',
+          800: '#13171f',
+          900: '#101319',
+        },
+      },
+      backgroundImage: (theme) => ({
+        check: "url('/icons/check.svg')",
+        landscape: "url('/images/landscape/2.jpg')",
+      }),
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['checked'],
+      borderColor: ['checked'],
+      inset: ['checked'],
+      zIndex: ['hover', 'active'],
+    },
   },
   plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
 };
